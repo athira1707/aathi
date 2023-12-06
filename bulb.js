991bfb24716f3d9f09734f6868f0af7d1cd37cb0
athira1707 <152839201+athira1707@users.noreply.github.com>
2023-12-06 12:34:36 +0530

    Add files via upload

---
+++ bulb.js
@@ -0,0 +1,9 @@
+
+
+function on(){
+document.getElementById("pic").src="https://th.bing.com/th/id/OIP.zHtYy800GhmmVF2UfkgymwHaJ4?w=129&h=180&c=7&r=0&o=5&pid=1.7"
+}  
+
+function off() {
+document.getElementById("pic").src="https://th.bing.com/th/id/OIP.cKsIqa8C2cv7vezivnpo4wHaJ4?w=142&h=189&c=7&r=0&o=5&pid=1.7"
+}
